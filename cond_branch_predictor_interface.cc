c2913f3a784cc996f83726da685d7f6ca1f23407
Saumya Patel <snp050106@gmail.com>
2026-02-02 19:14:33 -0700

    added the h and cppfiles too

--- cond_branch_predictor_interface.cc
+++ cond_branch_predictor_interface.cc
@@ -1,34 +1,43 @@
 /*
-  Copyright (C) ARM Limited 2008-2025  All rights reserved.                                                                                                                                                                                                                        
+  Copyright (C) ARM Limited 2008-2025  All rights reserved.
 
   All rights reserved.
 
-  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+  following conditions are met:
 
-  1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+  1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+  disclaimer.
 
-  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+  disclaimer in the documentation and/or other materials provided with the distribution.
 
-  3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+  3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote
+  products derived from this software without specific prior written permission.
 
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 // This file provides a sample predictor integration based on the interface provided.
 
-#include "lib/sim_common_structs.h"
 #include "cbp2016_tage_sc_l.h"
+#include "lib/sim_common_structs.h"
 #include "my_cond_branch_predictor.h"
 #include <cassert>
 
 //
 // beginCondDirPredictor()
-// 
+//
 // This function is called by the simulator before the start of simulation.
 // It can be used for arbitrary initialization steps for the contestant's code.
 //
-void beginCondDirPredictor()
-{
+void beginCondDirPredictor() {
     // setup sample_predictor
     cbp2016_tage_sc_l.setup();
     cond_predictor_impl.setup();
@@ -36,47 +45,49 @@ void beginCondDirPredictor()
 
 //
 // notify_instr_fetch(uint64_t seq_no, uint8_t piece, uint64_t pc, const uint64_t fetch_cycle)
-// 
-// This function is called when any instructions(not just branches) gets fetched.
-// Along with the unique identifying ids(seq_no, piece), PC of the instruction and fetch_cycle are also provided as inputs
 //
-void notify_instr_fetch(uint64_t seq_no, uint8_t piece, uint64_t pc, const uint64_t fetch_cycle)
-{
+// This function is called when any instructions(not just branches) gets fetched.
+// Along with the unique identifying ids(seq_no, piece), PC of the instruction and fetch_cycle are also provided as
+// inputs
+//
+void notify_instr_fetch(uint64_t seq_no, uint8_t piece, uint64_t pc, const uint64_t fetch_cycle) {
 }
 
 //
 // get_cond_dir_prediction(uint64_t seq_no, uint8_t piece, uint64_t pc, const uint64_t pred_cycle)
-// 
+//
 // This function is called by the simulator for predicting conditional branches.
 // input values are unique identifying ids(seq_no, piece) and PC of the branch.
-// return value is the predicted direction. 
+// return value is the predicted direction.
 //
-bool get_cond_dir_prediction(uint64_t seq_no, uint8_t piece, uint64_t pc, const uint64_t pred_cycle)
-{
-    const bool tage_sc_l_pred =  cbp2016_tage_sc_l.predict(seq_no, piece, pc);
-    const bool my_prediction = cond_predictor_impl.predict(seq_no, piece, pc, tage_sc_l_pred);
+bool get_cond_dir_prediction(uint64_t seq_no, uint8_t piece, uint64_t pc, const uint64_t pred_cycle) {
+    auto [tage_sc_l_pred, tage_high_conf] = cbp2016_tage_sc_l.predict(seq_no, piece, pc);
+    const bool my_prediction  = cond_predictor_impl.condbr_predict(seq_no, piece, pc, tage_sc_l_pred, tage_high_conf);
     return my_prediction;
 }
 
 //
-// spec_update(uint64_t seq_no, uint8_t piece, uint64_t pc, InstClass inst_class, const bool resolve_dir, const bool pred_dir, const uint64_t next_pc)
-// 
-// This function is called by the simulator for updating the history vectors and any state that needs to be updated speculatively.
-// The function is called for all the branches (not just conditional branches). To faciliate accurate history updates, spec_update is called right
-// after a prediction is made.
-// input values are unique identifying ids(seq_no, piece), PC of the instruction, instruction class, predicted/resolve direction and the next_pc 
+// spec_update(uint64_t seq_no, uint8_t piece, uint64_t pc, InstClass inst_class, const bool resolve_dir, const bool
+// pred_dir, const uint64_t next_pc)
 //
-void spec_update(uint64_t seq_no, uint8_t piece, uint64_t pc, InstClass inst_class, const bool resolve_dir, const bool pred_dir, const uint64_t next_pc)
-{
-    assert(is_br(inst_class));
+// This function is called by the simulator for updating the history vectors and any state that needs to be updated
+// speculatively. The function is called for all the branches (not just conditional branches). To faciliate accurate
+// history updates, spec_update is called right after a prediction is made. input values are unique identifying
+// ids(seq_no, piece), PC of the instruction, instruction class, predicted/resolve direction and the next_pc
+//
+void spec_update(uint64_t seq_no, uint8_t piece, uint64_t pc, InstClass inst_class, const bool resolve_dir,
+                 const bool pred_dir, const uint64_t next_pc) {
+    if (!is_br(inst_class)) {
+        return;
+    }
+
     int br_type = 0;
-    switch(inst_class)
-    {
+    switch (inst_class) {
         case InstClass::condBranchInstClass:
             br_type = 1;
             break;
         case InstClass::uncondDirectBranchInstClass:
-            br_type = 0; 
+            br_type = 0;
             break;
         case InstClass::uncondIndirectBranchInstClass:
             br_type = 2;
@@ -85,7 +96,7 @@ void spec_update(uint64_t seq_no, uint8_t piece, uint64_t pc, InstClass inst_cla
             br_type = 0;
             break;
         case InstClass::callIndirectInstClass:
-            br_type = 2; 
+            br_type = 2;
             break;
         case InstClass::ReturnInstClass:
             br_type = 2;
@@ -93,76 +104,81 @@ void spec_update(uint64_t seq_no, uint8_t piece, uint64_t pc, InstClass inst_cla
         default:
             assert(false);
     }
-
-    if(inst_class == InstClass::condBranchInstClass)
-    {
+    if (inst_class == InstClass::condBranchInstClass) {
         cbp2016_tage_sc_l.history_update(seq_no, piece, pc, br_type, pred_dir, resolve_dir, next_pc);
-        cond_predictor_impl.history_update(seq_no, piece, pc, resolve_dir, next_pc);
-    }
-    else
-    {
+    } else {
         cbp2016_tage_sc_l.TrackOtherInst(pc, br_type, pred_dir, resolve_dir, next_pc);
     }
-
+    cond_predictor_impl.spec_update(seq_no, piece, pc, inst_class, resolve_dir, pred_dir, next_pc);
 }
 
 //
-// notify_instr_decode(uint64_t seq_no, uint8_t piece, uint64_t pc, const DecodeInfo& _decode_info, const uint64_t decode_cycle)
-// 
+// notify_instr_decode(uint64_t seq_no, uint8_t piece, uint64_t pc, const DecodeInfo& _decode_info, const uint64_t
+// decode_cycle)
+//
 // This function is called when any instructions(not just branches) gets decoded.
-// Along with the unique identifying ids(seq_no, piece), PC of the instruction, decode info and cycle are also provided as inputs
+// Along with the unique identifying ids(seq_no, piece), PC of the instruction, decode info and cycle are also provided
+// as inputs
 //
 // For the sample predictor implementation, we do not leverage decode information
-void notify_instr_decode(uint64_t seq_no, uint8_t piece, uint64_t pc, const DecodeInfo& _decode_info, const uint64_t decode_cycle)
-{
+void notify_instr_decode(uint64_t seq_no, uint8_t piece, uint64_t pc, const DecodeInfo &_decode_info,
+                         const uint64_t decode_cycle) {
+    cond_predictor_impl.notify_instr_decode(seq_no, piece, pc, _decode_info, decode_cycle);
 }
 
 //
-// notify_agen_complete(uint64_t seq_no, uint8_t piece, uint64_t pc, const DecodeInfo& _decode_info, const uint64_t mem_va, const uint64_t mem_sz, const uint64_t agen_cycle)
-// 
+// notify_agen_complete(uint64_t seq_no, uint8_t piece, uint64_t pc, const DecodeInfo& _decode_info, const uint64_t
+// mem_va, const uint64_t mem_sz, const uint64_t agen_cycle)
+//
 // This function is called when any load/store instructions complete agen.
-// Along with the unique identifying ids(seq_no, piece), PC of the instruction, decode info, mem_va and mem_sz and agen_cycle are also provided as inputs
+// Along with the unique identifying ids(seq_no, piece), PC of the instruction, decode info, mem_va and mem_sz and
+// agen_cycle are also provided as inputs
 //
-void notify_agen_complete(uint64_t seq_no, uint8_t piece, uint64_t pc, const DecodeInfo& _decode_info, const uint64_t mem_va, const uint64_t mem_sz, const uint64_t agen_cycle)
-{
+void notify_agen_complete(uint64_t seq_no, uint8_t piece, uint64_t pc, const DecodeInfo &_decode_info,
+                          const uint64_t mem_va, const uint64_t mem_sz, const uint64_t agen_cycle) {
+    cond_predictor_impl.notify_agen_complete(seq_no, piece, pc, _decode_info, mem_va, mem_sz, agen_cycle);
 }
 
 //
-// notify_instr_execute_resolve(uint64_t seq_no, uint8_t piece, uint64_t pc, const bool pred_dir, const ExecuteInfo& _exec_info, const uint64_t execute_cycle)
-// 
+// notify_instr_execute_resolve(uint64_t seq_no, uint8_t piece, uint64_t pc, const bool pred_dir, const ExecuteInfo&
+// _exec_info, const uint64_t execute_cycle)
+//
 // This function is called when any instructions(not just branches) gets executed.
-// Along with the unique identifying ids(seq_no, piece), PC of the instruction, execute info and cycle are also provided as inputs
+// Along with the unique identifying ids(seq_no, piece), PC of the instruction, execute info and cycle are also provided
+// as inputs
 //
 // For conditional branches, we use this information to update the predictor.
-// At the moment, we do not consider updating any other structure, but the contestants are allowed to  update any other predictor state.
-void notify_instr_execute_resolve(uint64_t seq_no, uint8_t piece, uint64_t pc, const bool pred_dir, const ExecuteInfo& _exec_info, const uint64_t execute_cycle)
-{
+// At the moment, we do not consider updating any other structure, but the contestants are allowed to  update any other
+// predictor state.
+void notify_instr_execute_resolve(uint64_t seq_no, uint8_t piece, uint64_t pc, const bool pred_dir,
+                                  const ExecuteInfo &_exec_info, const uint64_t execute_cycle) {
     const bool is_branch = is_br(_exec_info.dec_info.insn_class);
-    if(is_branch)
-    {
-        if (is_cond_br(_exec_info.dec_info.insn_class))
-        {
+    if (is_branch) {
+        if (is_cond_br(_exec_info.dec_info.insn_class)) {
             const bool _resolve_dir = _exec_info.taken.value();
             const uint64_t _next_pc = _exec_info.next_pc;
             cbp2016_tage_sc_l.update(seq_no, piece, pc, _resolve_dir, pred_dir, _next_pc);
-            cond_predictor_impl.update(seq_no, piece, pc, _resolve_dir, pred_dir, _next_pc);
-        }
-        else
-        {
+        } else {
             assert(pred_dir);
         }
+    }
+    if (is_load(_exec_info.dec_info.insn_class)) {
+        cond_predictor_impl.notify_load_instr_execute_resolve(seq_no, piece, pc, pred_dir, _exec_info, execute_cycle);
     }
 }
 
 //
-// notify_instr_commit(uint64_t seq_no, uint8_t piece, uint64_t pc, const bool pred_dir, const ExecuteInfo& _exec_info, const uint64_t commit_cycle)
-// 
+// notify_instr_commit(uint64_t seq_no, uint8_t piece, uint64_t pc, const bool pred_dir, const ExecuteInfo& _exec_info,
+// const uint64_t commit_cycle)
+//
 // This function is called when any instructions(not just branches) gets committed.
-// Along with the unique identifying ids(seq_no, piece), PC of the instruction, execute info and cycle are also provided as inputs
+// Along with the unique identifying ids(seq_no, piece), PC of the instruction, execute info and cycle are also provided
+// as inputs
 //
 // For the sample predictor implementation, we do not leverage commit information
-void notify_instr_commit(uint64_t seq_no, uint8_t piece, uint64_t pc, const bool pred_dir, const ExecuteInfo& _exec_info, const uint64_t commit_cycle)
-{
+void notify_instr_commit(uint64_t seq_no, uint8_t piece, uint64_t pc, const bool pred_dir,
+                         const ExecuteInfo &_exec_info, const uint64_t commit_cycle) {
+    cond_predictor_impl.notify_instr_commit(seq_no, piece, pc, pred_dir, _exec_info, commit_cycle);
 }
 
 //
@@ -171,8 +187,7 @@ void notify_instr_commit(uint64_t seq_no, uint8_t piece, uint64_t pc, const bool
 // This function is called by the simulator at the end of simulation.
 // It can be used by the contestant to print out other contestant-specific measurements.
 //
-void endCondDirPredictor ()
-{
+void endCondDirPredictor() {
     cbp2016_tage_sc_l.terminate();
     cond_predictor_impl.terminate();
 }
